dc899f29afc9a12ca30a51efe33b6c58e7bfc36d
legendadi07 <148256947+legendadi07@users.noreply.github.com>
2023-10-29 20:55:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('ABHISHEK-SER', {
+say('LEGEND-PUSU', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @ABHISHEK SURESH._`, {
+say(`'${name}' By @LEGEND PUSU._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
