f513e869f0eff796cbd6df2b55920b8851c46f1f
legendadi07 <148256947+legendadi07@users.noreply.github.com>
2023-10-30 12:13:26 +0530

    Update tools-truecaller.js

--- plugins/tools-truecaller.js
+++ plugins/tools-truecaller.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn, text }) => {
     console.log('JSON response:', json);
 
     
-    json.creator = 'ABHISHEK SURESH';
+    json.creator = 'LEGEND PUSU';
 
     let milf = '';
     for (let prop in json) {
