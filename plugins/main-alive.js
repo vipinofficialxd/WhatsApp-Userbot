2c6f9815ba2bd68d61363343a3a44343a1ab3cde
legendadi07 <148256947+legendadi07@users.noreply.github.com>
2023-10-31 14:40:38 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
    let name = m.pushName || conn.getName(m.sender);
   
   // Define the content
-  let mainImg = "https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png"; // Main image URL
-  let smallImg = "https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png"; // Small image URL
+  let mainImg = "https://telegra.ph/file/7d7e3169e7b59e6f9983f.jpg"; // Main image URL
+  let smallImg = "https://telegra.ph/file/7d7e3169e7b59e6f9983f.jpg"; // Small image URL
   let smallText = "I'M Alive Now"; // Small text
-  let mainText = "𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁"; // Main text
-  let audioUrl = "https://raw.githubusercontent.com/AbhishekSuresh2/ABHISHEK-SER/main/src/mp3/Abhi.mp3"; // Audio URL
+  let mainText = "𝙻𝙴𝙶𝙴𝙽𝙳-𝙿𝚄𝚂𝚄"; // Main text
+  let audioUrl = "https://raw.githubusercontent.com/legendadi07/WhatsApp-Userbot/main/src/mp3/Abhi.mp3"; // Audio URL
   // Construct the message
   let con = {
     key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) },
@@ -26,14 +26,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     mimetype: 'audio/mp4',
     ptt: true,
     waveform: [100, 0, 100, 0, 100, 0, 100],
-    fileName: "ABHISHEK-SER",
+    fileName: "LEGEND-PUSU",
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
         title: smallText,
         body: mainText,
         thumbnailUrl: smallImg, // Small image
-        sourceUrl: 'https://github.com/AbhishekSuresh2/ABHISHEK-SER',
+        sourceUrl: 'https://telegra.ph/file/6adb9e9912e1fb27b41ec.mp4',
         mediaType: 1,
         renderLargerThumbnail: false,
         mediaUrl: mainImg // Main image
