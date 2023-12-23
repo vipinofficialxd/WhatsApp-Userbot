e28d66552132d01eed2bea8e09f69d5c3eb342c5
vipinofficialxd <120773322+vipinofficialxd@users.noreply.github.com>
2023-12-23 15:18:19 +0530

    Update main-ABHI.js

--- plugins/main-ABHI.js
+++ plugins/main-ABHI.js
@@ -1,21 +1,21 @@
 let handler = async m => m.reply(`
 
-≡  *LEGEND-AADITYA*   GROUPS
+≡  *LEGEND-VIPIN*   GROUPS
 ▢ Join Public Bot Group And Support
 https://chat.whatsapp.com/EIX0ozZ7Tv24NEX43KKOGW
 ─────────────
 ▢ *Owner Telegram*
-https://t.me/SEXYAADITYA
+https://t.me/BAAPXVIPIN
  
 ▢ *YouTube*
 https://youtube.com/@aadityaff1
 
 ▢ *Instagram*
-https://instagram.com/aadityagangster
+https://instagram.com/notyour_vipin
 
 `.trim())
-handler.help = ['gpadi']
+handler.help = ['gpvip']
 handler.tags = ['main']
-handler.command = ['groups', 'gpadi', 'adigp', 'groupadi'] 
+handler.command = ['groups', 'gpvip', 'vipgp', 'groupvip'] 
 
 export default handler
