031e88c64db9fa2a40b15705d3b230bdab21cfba
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-12-24 14:03:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ import axios from 'axios'
 
 global.owner = [
   ['919628779228', 'LEGEND-VIPIN', true],
-  ['919508888129'],
+  ['919508888129', 'LEGEND-AADITYA', true],
   [''] 
 ] //Number of owners
 
-global.mods = ['919628779228'] 
-global.prems = ['919628779228']
+global.mods = ['919628779228', '919508888129'] 
+global.prems = ['919628779228', '919508888129']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
